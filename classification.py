3dc375243bb41f621d421a48c280f37859415ea3
grzegorz <g.rypesc@wp.pl>
2019-06-27 15:28:47 +0200

    updated classification

--- classification.py
+++ classification.py
@@ -5,6 +5,7 @@ from sklearn.svm import LinearSVC
 from sklearn.neural_network import MLPClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.neighbors import KNeighborsClassifier
 
 path = "./input/"
 input = pd.read_csv(path + 'dataPostProcessed.csv', index_col=0)
@@ -13,18 +14,19 @@ from sklearn.model_selection import train_test_split
 X = input['headline']#.append(df['headline'])
 Y = input['is_sarcastic']#.append(df['is_sarcastic'])
 
-xTrain, xTest, yTrain, yTest = train_test_split(X, Y, test_size=0.1, random_state=42)
+xTrain, xTest, yTrain, yTest = train_test_split(X, Y, test_size=0.05, random_state=42)
 classifiers = [
-    #SVC = Pipeline([('tfidf', TfidfVectorizer()), ('clf', LinearSVC())]),
-    Pipeline([('tfidf', TfidfVectorizer()), ('clf', MLPClassifier(hidden_layer_sizes=(300,200), random_state=42, warm_start=True, solver='adam'))]),
-    #RFC = Pipeline([('tfidf', TfidfVectorizer()), ('clf', RandomForestClassifier())]),
+    Pipeline([('tfidf', TfidfVectorizer()), ('classifier', LinearSVC())]),
+    Pipeline([('tfidf', TfidfVectorizer()), ('classifier', MLPClassifier(hidden_layer_sizes=(80,80), random_state=42, solver='lbfgs'))]),
+    Pipeline([('tfidf', TfidfVectorizer()), ('classifier', RandomForestClassifier())]),
+    Pipeline([('tfidf', TfidfVectorizer()), ('classifier', KNeighborsClassifier(n_neighbors=2))]),
     ]
-
 
 for classifier in classifiers:
     classifier.fit(xTrain, yTrain)
-    print("### " + classifier.__class__.__name__ + " ###")
-    print("Classification accuracy: " + str(classifier.score(xTest, yTest)) +"\n")
+    print("### " + classifier.named_steps.get("classifier").__class__.__name__ + " ###")
+    print("\nTraining: " + str(classifier.score(xTrain, yTrain).round(3)))
+    print("Test: " + str(classifier.score(xTest, yTest).round(3)) +"\n")
     prediction = classifier.predict(xTest)
     print(confusion_matrix(yTest, prediction))
-    print(classification_report(yTest, pred))
+    print(classification_report(yTest, prediction))
